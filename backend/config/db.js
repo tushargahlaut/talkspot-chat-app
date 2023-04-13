68e3008fcfacf60287a4e08da6638bfdcae265f9
Tushar Gahlaut <tushargahlaut74@gmail.com>
2023-04-13 16:02:09 +0530

    Update DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
+  console.log("MONGO URL HERE", process.env.MONGO_URI);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
