6d22c5f12d9f63e77d2ab79d7282d2a019ddcd74
Tushar Gahlaut <tushargahlaut74@gmail.com>
2023-04-23 12:56:53 +0530

    Secure Mongo URL

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, "SECRET", {
+  return jwt.sign({ id }, process.env.JWT_SEC, {
     expiresIn: "30d",
   });
 };
